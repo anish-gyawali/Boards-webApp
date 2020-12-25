70e268a97dc57200bb671743fd09c432ac56183e
Anish Gyawali <anish.gyawali@selu.edum>
2020-12-25 16:16:50 -0600

    created the first models, migrated the database

--- DjangoProject/BoardsProject/Boards/views.py
+++ DjangoProject/BoardsProject/Boards/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
 
 
 def home(request):
-    return HttpResponse('here we can start our project')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
